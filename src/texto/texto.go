2e51f14dc3f1a30e67243bcbf6a5552bf989346c
Guillermo Lupiañez Tapia <guillelupianez99@gmail.com>
2020-10-13 11:36:07 +0200

    Organizado proyecto

---
+++ src/texto/texto.go
@@ -0,0 +1,68 @@
+package texto
+
+import (
+	"fmt"
+)
+
+type texto struct {
+	contenido    string
+	repeticiones map[string]int
+	autor        string
+}
+
+func (t *texto) NewTexto(c string, r map[string]int, a string) *texto {
+	t.contenido = c
+	t.repeticiones = r
+	t.autor = a
+
+	return t
+}
+func (t *texto) GetContenido() string {
+	return t.contenido
+}
+
+func (t *texto) GetRepeticiones() map[string]int {
+	return t.repeticiones
+}
+
+func (t *texto) GetAutor() string {
+	return t.autor
+}
+
+func (t *texto) SetContenido(contenido string) {
+	t.contenido = contenido
+}
+
+func (t *texto) SetRepeticiones(repeticiones map[string]int) {
+	t.repeticiones = repeticiones
+}
+
+func (t *texto) SetAutor(autor string) {
+	t.autor = autor
+}
+
+func (t *texto) ObtenerRedundantes() string {
+	texto := limpiar(contenido)
+
+	redundantes := make([]string, 1)
+
+	return redundantes
+}
+
+func (t *texto) IntroducirTexto(texto []string) {
+	fmt.Println("Not implemented yet")
+}
+
+func (t *texto) GetReferencias() []string {
+	s := make([]string, 1)
+
+	fmt.Println("Not implemented yet")
+	return s
+}
+
+func (t *texto) GetTextoPersona() []string {
+	s := make([]string, 1)
+
+	fmt.Println("Not implemented yet")
+	return s
+}
---
+++ src/texto/texto_test.go
@@ -0,0 +1,7 @@
+package texto
+
+import "testing"
+
+func TestObtenerRedundantes(t *testing.T) {
+	texto := "Mi nombre es Guillermo Lupiáñez. veces practico"
+}
