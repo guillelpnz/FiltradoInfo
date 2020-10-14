16a245ad4e21a39764a09f0fa71e60d845a71a4e
Guillermo Lupiañez Tapia <guillelupianez99@gmail.com>
2020-10-14 11:04:34 +0200

    Prueba testlimpiar #18

--- src/funcs_test.go
+++ src/funcs_test.go
@@ -3,14 +3,22 @@ package texto
 import "testing"
 
 func TestContains(t *testing.T) {
-	cadena := "Hola!"
+	var cadena string
+	cadena = "Hola!"
 
 	if !contains(cadena, '!') {
 		t.Error("Contains no funciona adecuadamente")
-	} else {
-		t.Log("Contains funciona adecuadamente")
 	}
 }
 func TestLimpiar(t *testing.T) {
+	var cadena, correcta string
 
+	cadena = "¡¡Hola!! Mi nombre es Guille."
+	correcta = "Hola Mi nombre es Guille"
+
+	cadena = limpiar(cadena)
+
+	if cadena != correcta {
+		t.Error("Limpiar no funciona adecuadamente")
+	}
 }
