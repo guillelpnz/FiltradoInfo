a9c054739fa6c0f7e968816905c39cb463ef6947
Guillermo Lupiañez Tapia <guillelupianez99@gmail.com>
2020-10-14 10:48:39 +0200

    Subido test contains #18

--- src/funcs_test.go
+++ src/funcs_test.go
@@ -3,7 +3,13 @@ package texto
 import "testing"
 
 func TestContains(t *testing.T) {
+	cadena := "Hola!"
 
+	if !contains(cadena, '!') {
+		t.Error("Contains no funciona adecuadamente")
+	} else {
+		t.Log("Contains funciona adecuadamente")
+	}
 }
 func TestLimpiar(t *testing.T) {
 
