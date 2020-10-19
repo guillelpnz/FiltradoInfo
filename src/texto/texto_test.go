fd1373a947993be4ae2574cc40cf3f1a8bc17449
Guillermo Lupiañez Tapia <guillelupianez99@gmail.com>
2020-10-19 17:31:35 +0200

    Elimina las funciones que hacen test para corregir la rubrica 2

--- src/texto/texto_test.go
+++ src/texto/texto_test.go
@@ -4,38 +4,3 @@ import (
 	"testing"
 )
 
-func TestObtenerSinRedundantes(t *testing.T) {
-	var mapa map[string]int
-	var autor string
-
-	cadena := "Mi nombre es Guillermo Lupiáñez. A veces practico pádel, otras veces practico Waterpolo"
-	esperada := "Mi nombre es Guillermo Lupiáñez. A veces practico pádel, otras Waterpolo"
-	sliceEsperada := StringToSlice(esperada)
-
-	text := Texto{contenido: cadena, repeticiones: mapa, autor: autor}
-
-	for i, palabra := range text.ObtenerSinRedundantes() {
-		if palabra != sliceEsperada[i] {
-			t.Error("Obtener redundantes mal implementado")
-			break
-		}
-	}
-}
-
-func TestObtenerRedundantes(t *testing.T) {
-	// var mapa map[string]int
-	// var autor string
-
-	cadena := "Mi nombre es Guillermo Lupiáñez. A veces practico pádel, otras veces practico Waterpolo"
-	esperada := "veces practico"
-	sliceEsperada := StringToSlice(esperada)
-
-	text := NewTexto(cadena, nil, "")
-
-	for i, palabra := range text.ObtenerRedundantes() {
-		if palabra != sliceEsperada[i] {
-			t.Error("Obtener redundantes mal implementado")
-			break
-		}
-	}
-}
