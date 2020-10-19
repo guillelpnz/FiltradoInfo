8f660e4b7d383d0ca1040e39ae7d23d043f0c7d9
Guillermo Lupiañez Tapia <guillelupianez99@gmail.com>
2020-10-19 17:34:32 +0200

    hecho test de la funcion auxiliar Contains #7 #19

--- src/texto/funcs_test.go
+++ src/texto/funcs_test.go
@@ -2,3 +2,11 @@ package texto
 
 import "testing"
 
+func TestContains(t *testing.T) {
+	var cadena string
+	cadena = "Hola!"
+
+	if !Contains(cadena, '!') {
+		t.Error("Contains no funciona adecuadamente")
+	}
+}
