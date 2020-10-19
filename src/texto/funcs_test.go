9ad2f8549c38433503b24903a57fea7b3dc82001
Guillermo Lupiañez Tapia <guillelupianez99@gmail.com>
2020-10-19 17:35:11 +0200

    Hecha la funcion que testea Limpiar #7 #19

--- src/texto/funcs_test.go
+++ src/texto/funcs_test.go
@@ -10,3 +10,16 @@ func TestContains(t *testing.T) {
 		t.Error("Contains no funciona adecuadamente")
 	}
 }
+
+func TestLimpiar(t *testing.T) {
+	var cadena, correcta string
+
+	cadena = "¡¡Hola!! Mi nombre es Guille.."
+	correcta = "Hola Mi nombre es Guille"
+
+	cadena = Limpiar(cadena)
+
+	if cadena != correcta {
+		t.Error("Limpiar no funciona adecuadamente")
+	}
+}
