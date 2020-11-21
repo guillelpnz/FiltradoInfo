516608bdacc7d1aa2dee06dc6bf429284ffffb82
Guillermo Lupiañez Tapia <guillelupianez99@gmail.com>
2020-11-21 18:01:07 +0100

    Avanza funcion handler #35

---
+++ api/obtener-sin-redundantes.go
@@ -0,0 +1,30 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/guillelpnz/TextAnalyzer/src/texto"
+)
+
+// Handler returns a webpage
+func Handler(w http.ResponseWriter, r *http.Request) {
+
+	switch r.Method {
+
+	case "POST":
+		r.ParseForm()
+
+		textoForm := r.FormValue("texto")
+		textoObj := texto.NewTextoRep(textoForm, "")
+
+		for _, palabra := range textoObj.ObtenerSinRedundantes() {
+			fmt.Fprintf(w, palabra)
+		}
+
+		break
+	default:
+		http.ServeFile(w, r, "templates/index.html")
+		break
+	}
+}
