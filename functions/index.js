dc46995fd1665a38e504adbc36611d948bda193c
Guillermo Lupiañez Tapia <guillelupianez99@gmail.com>
2020-11-23 19:51:14 +0100

    Decide hacer bot en Netlify #38

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,4 @@
+const request = require('request-promise');
 exports.handler = async event => {
   const data = {
     "poetas": {
@@ -21,17 +22,29 @@ exports.handler = async event => {
     }
   }
 
+  
+  const body = JSON.parse(event.body);
+  let {conver, texto} = body.message;
+
   let result = ""
-  const autor = event.queryStringParameters.autor || '';
-  if (autor == ''){
-    result = "Poemas de Federico García Lorca: " + data["poetas"]["Federico García Lorca"]["poemas"].toString()
-  }
-  else{
-    result = "Poemas de Rafael Alberti: " + data["poetas"]["Rafael Alberti"]["poemas"].toString()
+
+  switch(texto){
+    case "alberti":
+      result = "Poemas de Rafael Alberti: " + data["poetas"]["Rafael Alberti"]["poemas"].toString();
+      break;
+    
+    case "lorca":
+      result = "Poemas de Federico García Lorca: " + data["poetas"]["Federico García Lorca"]["poemas"].toString();
+      break;
+    default:
+      result = "Use /alberti o /lorca para conocer sus poemas más famosos"
   }
 
   return {
     statusCode: 200,
-    body: result
+    body: JSON.stringify({texto:result, method:'sendMessage', chat_id:conver.id}),
+    headers:{
+        'Content-Type': 'application/json'
+    }
   }
 }
