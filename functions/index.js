fc961f473c4287b7f8bb52ba47e0e64169c014b2
Guillermo Lupiañez Tapia <guillelupianez99@gmail.com>
2020-11-23 18:00:30 +0100

    Intenta corregir fallo #10 #37 #38

--- functions/index.js
+++ functions/index.js
@@ -1,40 +1,30 @@
-const data = {
+const request = require('request-promise');
+let data = {
   "poetas": {
-      "Federico García Lorca": {
-          "poemas": [
-              "Casida del Llanto",
-              "Casada infiel",
-              "Asesinato",
-              "Soneto de la dulce queja",
-              "Danza da lúa en Santiago"
-          ]
-      },
-      "Rafael Alberti": {
-          "poemas": [
-              "Amaranta",
-              "Nocturno",
-              "La paloma",
-              "A galopar"
-          ]
-      }
+    "Federico García Lorca": {
+        "poemas": [
+            "Casida del Llanto",
+            "Casada infiel",
+            "Asesinato",
+            "Soneto de la dulce queja",
+            "Danza da lúa en Santiago"
+        ]
+    },
+    "Rafael Alberti": {
+        "poemas": [
+            "Amaranta",
+            "Nocturno",
+            "La paloma",
+            "A galopar"
+        ]
+    }
   }
 }
-function devolverPoemas(autor){
-  let poemas = data["poetas"][autor]["poemas"]
-
-  if (poemas != ""){
-    return poemas
-  }
-}
-
-console.log(devolverPoemas("Federico García Lorca"))
 
 exports.handler = async event => {
   const autor = event.queryStringParameters.autor || '';
-
-  let result = ''
   if (autor != ''){
-    result = devolverPoemas(autor.toString())
+    result = data["poetas"][autor.toString()]["poemas"]
   }
 
   return {
