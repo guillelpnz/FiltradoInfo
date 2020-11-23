e7ec34c0afd0f65b641b49b1059958d6dbf8a62a
Guillermo Lupiañez Tapia <guillelupianez99@gmail.com>
2020-11-23 17:48:32 +0100

    Intenta corregir fallo #10 #37 #38

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,24 @@
-const data = require("./poemas.json");
+const data = {
+  "poetas": {
+      "Federico García Lorca": {
+          "poemas": [
+              "Casida del Llanto",
+              "Casada infiel",
+              "Asesinato",
+              "Soneto de la dulce queja",
+              "Danza da lúa en Santiago"
+          ]
+      },
+      "Rafael Alberti": {
+          "poemas": [
+              "Amaranta",
+              "Nocturno",
+              "La paloma",
+              "A galopar"
+          ]
+      }
+  }
+}
 function devolverPoemas(autor){
   let poemas = data["poetas"][autor]["poemas"]
 
