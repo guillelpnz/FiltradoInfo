7bf2b82b0eb765ea7b78d7e6ef797fc7e25674a2
Guillermo Lupiañez Tapia <guillelupianez99@gmail.com>
2020-11-23 09:23:38 +0100

    Hace prueba con variable get #35 #19

--- api/obtener-sin-redundantes.go
+++ api/obtener-sin-redundantes.go
@@ -1,12 +1,8 @@
 package handler
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"net/http"
-
-	"github.com/guillelpnz/TextAnalyzer/src/texto"
 )
 
 // Respuesta contains a text without duplicate words
@@ -24,28 +20,34 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 	// var result Peticion
 
-	body, _ := ioutil.ReadAll(r.Body)
+	//body, _ := ioutil.ReadAll(r.Body)
 
 	// if err := json.Unmarshal(body, &result); err != nil {
 	// 	log.Fatal("Error desserializando json-> ", err)
 	// }
 
-	result := string(body)
-
-	fmt.Fprintf(w, fmt.Sprintf("Result -> %s", result))
-
-	textoObj := texto.NewTextoRep(result, "")
-	contenidoSinR := ""
-
-	for _, palabra := range textoObj.ObtenerSinRedundantes() {
-		contenidoSinR += palabra + " "
+	switch r.Method {
+	case "GET":
+		for k, v := range r.URL.Query() {
+			fmt.Fprintf(w, "%s: %s\n", k, v)
+		}
+		break
 	}
 
-	respSinSerializar := Respuesta{Contenido: contenidoSinR}
+	// fmt.Fprintf(w, fmt.Sprintf("Result -> %s", result))
 
-	respSerializada, _ := json.Marshal(respSinSerializar)
+	// textoObj := texto.NewTextoRep(result, "")
+	// contenidoSinR := ""
 
-	w.Header().Add("Content-Type", "application/json")
+	// for _, palabra := range textoObj.ObtenerSinRedundantes() {
+	// 	contenidoSinR += palabra + " "
+	// }
 
-	fmt.Fprintf(w, "Texto sin repetidos -> "+string(respSerializada))
+	// respSinSerializar := Respuesta{Contenido: contenidoSinR}
+
+	// respSerializada, _ := json.Marshal(respSinSerializar)
+
+	// w.Header().Add("Content-Type", "application/json")
+
+	// fmt.Fprintf(w, "Texto sin repetidos -> "+string(respSerializada))
 }
