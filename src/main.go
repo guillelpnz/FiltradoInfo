bac57ae5222dc278ab6251e9cc7b96bb7779c4e9
Guillermo Lupiañez Tapia <guillelupianez99@gmail.com>
2020-12-08 15:28:57 +0100

    Prueba api #42

--- src/main.go
+++ src/main.go
@@ -3,17 +3,37 @@ package main
 import (
 	// "fmt"
 
-	// "github.com/guillelpnz/TextAnalyzer/src/texto"
+	"github.com/guillelpnz/TextAnalyzer/src/micro"
 
 	"github.com/gin-gonic/gin"
 )
 
+var textos *micro.Textos
+
+func introducirTexto(c *gin.Context) {
+	contenido := c.PostForm("contenido")
+	autor := c.PostForm("autor")
+
+	textos.IntroducirTexto(contenido, autor)
+
+	c.JSON(200, gin.H{
+		"mensaje":   "Texto añadido con éxito",
+		"contenido": contenido,
+		"autor":     autor,
+	})
+}
+
+// go get -u github.com/gin-gonic/gin
 func main() {
+	textos = micro.NewTextos()
+
 	router := gin.Default()
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
+
+	router.POST("/introducir-texto", introducirTexto)
 	router.Run() // listen and serve on 0.0.0.0:8080
 }
